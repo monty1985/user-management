cff27920dbb5436712ceb21a789021ae7f449b0d
karthmoh <karthmoh@USkarthmoh01.microfocus.com>
2019-03-26 18:33:49 -0700

    task#0000 update the constants class

---
+++ user-management/src/main/java/com/usrmgt/spring/utils/CONSTANTS.java
@@ -0,0 +1,13 @@
+package com.usrmgt.spring.utils;
+
+public class CONSTANTS {
+	
+	public static final String dbName = "usermanagerdb";
+	public static final String dbtableName = "users";
+	public static final String dbColfirstName = "first_name";
+	public static final String dbColLastNameDB = "last_name";
+	public static final String dbColEmailDB = "email_id";
+	public static final String dbColUserID = "userid";
+	public static final String dbColAccess = "access";
+
+}
